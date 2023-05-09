19ee1a7631addc7572ceedff7bbc7a5a07a543ec
patrik.hultgren <info@patrikhultgren.se>
2023-05-09 10:47:03 +0200

    Use env variables

--- src/lib/services/faboul/endpoints.ts
+++ src/lib/services/faboul/endpoints.ts
@@ -1,9 +1,8 @@
-// const FABOUL_API_URL = 'https://sholiday.faboul.se/dagar/v2.1'
-const FABOUL_API_URL = '/api/days'
+import { PUBLIC_FABOUL_API_URL } from '$env/static/public'
 
 const endpoints = {
 	getMonthUrl: (year: number, month: number): string =>
-		`${FABOUL_API_URL}/${year}/${month.toString().padStart(2, '0')}`
+		`${PUBLIC_FABOUL_API_URL}/${year}/${month.toString().padStart(2, '0')}`
 }
 
 export default endpoints
