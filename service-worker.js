5f7d34ca76a8e17743f170f767be5bd264a8e44a
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 18:17:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l = [
-  a + "/_app/immutable/entry/app.5fc85af2.js",
+  a + "/_app/immutable/entry/app.55e05afa.js",
   a + "/_app/immutable/assets/0.a5c7ca8b.css",
   a + "/_app/immutable/nodes/0.4c520608.js",
   a + "/_app/immutable/assets/sofiapro-light-webfont.44cf56d3.woff2",
   a + "/_app/immutable/assets/sofiapro-light-webfont.e8624630.woff",
-  a + "/_app/immutable/nodes/1.a94c711c.js",
-  a + "/_app/immutable/nodes/2.a2776e5d.js",
+  a + "/_app/immutable/nodes/1.502d618b.js",
+  a + "/_app/immutable/nodes/2.1f5153e5.js",
   a + "/_app/immutable/assets/Container.6bf551a2.css",
   a + "/_app/immutable/chunks/Container.65bcfbe7.js",
   a + "/_app/immutable/chunks/index.f51fc106.js",
-  a + "/_app/immutable/chunks/paths.e1dfd961.js",
-  a + "/_app/immutable/chunks/singletons.4d229c24.js",
-  a + "/_app/immutable/entry/start.bf9f2815.js"
+  a + "/_app/immutable/chunks/paths.c9c543d2.js",
+  a + "/_app/immutable/chunks/singletons.2ea12a81.js",
+  a + "/_app/immutable/entry/start.6a759e7a.js"
 ], d = [
   a + "/.nojekyll",
   a + "/android-chrome-192x192.png",
@@ -29,7 +29,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l
   a + "/favicon.ico",
   a + "/robots.txt",
   a + "/site.webmanifest"
-], f = "1683734355396", c = `cache-${f}`, p = [
+], r = "1683735424988", c = `cache-${r}`, p = [
   ...l,
   // the app itself
   ...d
