086cfbb9bbaa12e54c376f3beb75e297f480d5ee
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 07:16:34 +0200

    Define months

---
+++ src/lib/config/index.ts
@@ -0,0 +1,14 @@
+export const MONTHS: Array<string> = [
+	'januari',
+	'februari',
+	'mars',
+	'april',
+	'maj',
+	'juni',
+	'juli',
+	'augusti',
+	'september',
+	'oktober',
+	'november',
+	'december'
+]
