90e9e5688094bd6a65d98e6171f3f956b4684f78
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 12:07:05 +0200

    Enable service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
 	import '$lib/css/style.css'
+	import { goto } from '$app/navigation'
+	import { browser } from '$app/environment'
+	import { page } from '$app/stores'
+	import { onMount, onDestroy } from 'svelte'
+
+	function onVisibilityChange() {
+		if (page.url === '/') {
+			window.location.reload()
+		} else {
+			goto('/')
+		}
+	}
+
+	onMount(() => {
+		document.addEventListener('visibilitychange', onVisibilityChange)
+	})
+
+	onDestroy(() => {
+		if (browser) {
+			document.removeEventListener('visibilitychange', onVisibilityChange)
+		}
+	})
 </script>
 
 <slot />
