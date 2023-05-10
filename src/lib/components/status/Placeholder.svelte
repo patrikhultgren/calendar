663746656b7727a1cb5f8a1d61d45f871b0644b7
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 08:54:31 +0200

    Placeholder styling on mobile

---
+++ src/lib/components/status/Placeholder.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import classNames from 'classnames'
+
+	export let className = ''
+</script>
+
+<div class={classNames('placeholder', className)}>
+	<div class={classNames('placeholder__animated-background', className)} />
+</div>
