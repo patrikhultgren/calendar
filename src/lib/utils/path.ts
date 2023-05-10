ff9cbec7a22ff8054f2a8404aa0ba4fc59c405da
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 16:04:12 +0200

    Add to GitHub pages

--- src/lib/utils/path.ts
+++ src/lib/utils/path.ts
@@ -1,7 +1,8 @@
 import { months } from '$lib/config'
 import { format } from '$lib/utils/date'
+import { base } from '$app/paths'
 
 export const getMonthPath = (date: Date, now: Date): string =>
 	format(date, 'yyyy-MM') === format(now, 'yyyy-MM')
-		? '/'
-		: `/${date.getFullYear()}/${months[date.getMonth()]}`
+		? `${base}/`
+		: `${base}/${date.getFullYear()}/${months[date.getMonth()]}`
