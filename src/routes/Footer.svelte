501b6704d4e06aee1b5a9a7abc843a310c89909b
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 23:22:50 +0200

    Update footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -26,22 +26,21 @@
 			<div
 				bind:this={footerInfo}
 				id="footer-info"
-				class="px-4 [&>p]:mt-4 first:[&>p]:mt-0 mt-8 mx-auto max-w-[600px]"
+				class="px-4 [&>p]:mt-2 first:[&>p]:mt-0 mt-8 mx-auto max-w-[600px] text-center"
 			>
-				<h2 class="text-lg">
-					Denna sajt är till för att enkelt kunna se helgdagar och veckonummer.
-				</h2>
+				<h2 class="text-2xl font-bold">Se helgdagar och veckonummer enkelt</h2>
+				<p>Lägg gärna till sajten som en ikon på din hemskärm i mobilen.</p>
 				<p>
-					Kalenderdatan tillhandahålls av <a class="underline" href="https://sholiday.faboul.se/">
+					Sidan är byggd av {' '}
+					<a class="underline" href="mailto:patrik.hult@gmail.com"> Patrik Hultgren</a>.
+				</p>
+				<p class="text-xs mt-6">
+					Innehållet tillhandahålls av <a class="underline" href="https://sholiday.faboul.se/">
 						Faboul</a
 					>.
 				</p>
-				<p>Inga kakor används på sajten.</p>
-				<p>Innehållet kan vara felaktigt. Inget ansvar tas kring innehållet.</p>
-				<p>
-					Sidan är byggd av mig{' '}
-					<a class="underline" href="mailto:patrik.hult@gmail.com"> Patrik Hultgren</a>.
-				</p>
+				<p class="text-xs">Inga kakor används på sajten.</p>
+				<p class="text-xs">Innehållet kan vara felaktigt. Inget ansvar tas kring innehållet.</p>
 			</div>
 		{/if}
 	</Container>
