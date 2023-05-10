da67e5342f4d5462267121fd4aac59eee667ae66
patrik.hultgren <info@patrikhultgren.se>
2023-05-10 21:43:23 +0200

    Format dates

--- src/lib/components/calendar/Header.svelte
+++ src/lib/components/calendar/Header.svelte
@@ -33,16 +33,22 @@
 				class="flex py-4 px-1 sm:px-4 items-center capitalize rounded sm:text-xl font-bold"
 				href={previousMonthPath}
 			>
-				<Arrow direction="left" />{format(previousMonth, 'MMMM')}
+				<Arrow direction="left" />
+				<span class="inline sm:hidden">{format(previousMonth, 'MMM')}</span>
+				<span class="hidden sm:inline">{format(previousMonth, 'MMMM')}</span>
 			</a>
 			<h1 class="flex items-center capitalize px-4 text-2xl sm:text-4xl font-bold">
-				<span class="inline sm:hidden">{format(currentMonth, 'MMM yyyy').replace('.', '')}</span>
+				<span class="inline sm:hidden">{format(currentMonth, 'MMM yyyy')}</span>
 				<span class="hidden sm:inline">{format(currentMonth, 'MMMM yyyy')}</span>
 			</h1>
 			<a
 				class="flex py-4 px-1 sm:px-4 items-center capitalize rounded sm:text-xl font-bold"
-				href={nextMonthPath}>{format(nextMonth, 'MMMM')}<Arrow direction="right" /></a
+				href={nextMonthPath}
 			>
+				<span class="inline sm:hidden">{format(nextMonth, 'MMM')}</span>
+				<span class="hidden sm:inline">{format(nextMonth, 'MMMM')}</span>
+				<Arrow direction="right" />
+			</a>
 		</nav>
 	</Container>
 </header>
