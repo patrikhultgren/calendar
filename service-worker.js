86800dc50e94743dc370754655d6ddc2f5555fd6
patrik.hultgren <info@patrikhultgren.se>
2023-05-11 12:25:35 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l = [
-  a + "/_app/immutable/entry/app.cbc3d645.js",
+  a + "/_app/immutable/entry/app.399213e6.js",
   a + "/_app/immutable/assets/0.191d929b.css",
   a + "/_app/immutable/nodes/0.637500aa.js",
   a + "/_app/immutable/assets/sofiapro-light-webfont.44cf56d3.woff2",
   a + "/_app/immutable/assets/sofiapro-light-webfont.e8624630.woff",
-  a + "/_app/immutable/nodes/1.367fe82d.js",
-  a + "/_app/immutable/nodes/2.6651fdd7.js",
+  a + "/_app/immutable/nodes/1.54899df8.js",
+  a + "/_app/immutable/nodes/2.a6c6b650.js",
   a + "/_app/immutable/assets/Container.6bf551a2.css",
   a + "/_app/immutable/chunks/Container.9a7d2acd.js",
   a + "/_app/immutable/chunks/index.1b23dfab.js",
   a + "/_app/immutable/chunks/index.e351a105.js",
-  a + "/_app/immutable/chunks/singletons.b11ca8c1.js",
-  a + "/_app/immutable/chunks/stores.7edf088f.js",
-  a + "/_app/immutable/entry/start.e71ae308.js"
-], d = [
+  a + "/_app/immutable/chunks/singletons.b14576ca.js",
+  a + "/_app/immutable/chunks/stores.a0ec86a8.js",
+  a + "/_app/immutable/entry/start.a8775a18.js"
+], r = [
   a + "/.nojekyll",
   a + "/android-chrome-192x192.png",
   a + "/android-chrome-512x512.png",
@@ -31,41 +31,42 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l
   a + "/logo.html",
   a + "/robots.txt",
   a + "/site.webmanifest"
-], r = "1683800252719", c = `cache-${r}`, p = [
-  ...l,
-  // the app itself
-  ...d
-  // everything in `static`
-];
-self.addEventListener("install", (e) => {
-  async function t() {
-    await (await caches.open(c)).addAll(p), self.skipWaiting();
-  }
-  e.waitUntil(t());
-});
-self.addEventListener("activate", (e) => {
-  async function t() {
-    for (const s of await caches.keys())
-      s !== c && await caches.delete(s);
-  }
-  e.waitUntil(t());
-});
-self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET")
-    return;
-  async function t() {
-    const s = new URL(e.request.url), n = await caches.open(c);
-    if (p.includes(s.pathname))
-      return n.match(s.pathname);
-    const o = await n.match(e.request);
-    if (o)
-      return o;
-    try {
-      const i = await fetch(e.request);
-      return i.status === 200 && n.put(e.request, i.clone()), i;
-    } catch {
+], d = "1683800723044";
+if (a) {
+  const n = `cache-${d}`, o = [
+    ...l,
+    // the app itself
+    ...r
+    // everything in `static`
+  ];
+  self.addEventListener("install", (e) => {
+    async function t() {
+      await (await caches.open(n)).addAll(o), self.skipWaiting();
     }
-    return null;
-  }
-  e.respondWith(t());
-});
+    e.waitUntil(t());
+  }), self.addEventListener("activate", (e) => {
+    async function t() {
+      for (const s of await caches.keys())
+        s !== n && await caches.delete(s);
+    }
+    e.waitUntil(t());
+  }), self.addEventListener("fetch", (e) => {
+    if (e.request.method !== "GET")
+      return;
+    async function t() {
+      const s = new URL(e.request.url), i = await caches.open(n);
+      if (o.includes(s.pathname))
+        return i.match(s.pathname);
+      const p = await i.match(e.request);
+      if (p)
+        return p;
+      try {
+        const c = await fetch(e.request);
+        return c.status === 200 && i.put(e.request, c.clone()), c;
+      } catch {
+      }
+      return null;
+    }
+    e.respondWith(t());
+  });
+}
