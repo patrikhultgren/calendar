9733d610e7c730e524387c8c2e20a27b34ff3dc5
patrik.hultgren <info@patrikhultgren.se>
2023-05-26 10:47:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l = [
-  a + "/_app/immutable/entry/app.5204bf15.js",
-  a + "/_app/immutable/assets/0.65e54d77.css",
-  a + "/_app/immutable/nodes/0.7d68179d.js",
+  a + "/_app/immutable/entry/app.ebaa7958.js",
+  a + "/_app/immutable/assets/0.d7bba26b.css",
+  a + "/_app/immutable/nodes/0.cdaf1856.js",
   a + "/_app/immutable/assets/sofiapro-light-webfont.44cf56d3.woff2",
   a + "/_app/immutable/assets/sofiapro-light-webfont.e8624630.woff",
-  a + "/_app/immutable/nodes/1.1018d3dd.js",
+  a + "/_app/immutable/nodes/1.3025c9d6.js",
   a + "/_app/immutable/assets/2.6bf551a2.css",
-  a + "/_app/immutable/nodes/2.cbdf695e.js",
+  a + "/_app/immutable/nodes/2.d1eb207d.js",
   a + "/_app/immutable/chunks/Container.8a16cb38.js",
   a + "/_app/immutable/chunks/index.e0d45c7b.js",
   a + "/_app/immutable/chunks/index.f7c07964.js",
-  a + "/_app/immutable/chunks/singletons.be812e46.js",
-  a + "/_app/immutable/chunks/stores.83f6c390.js",
-  a + "/_app/immutable/entry/start.54877777.js"
+  a + "/_app/immutable/chunks/singletons.176092c9.js",
+  a + "/_app/immutable/chunks/stores.c1a60d35.js",
+  a + "/_app/immutable/entry/start.75de2687.js"
 ], d = [
   a + "/.nojekyll",
   a + "/android-chrome-192x192.png",
@@ -31,7 +31,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l
   a + "/logo.html",
   a + "/robots.txt",
   a + "/site.webmanifest"
-], m = "1684393974055";
+], m = "1685090871811";
 if (a) {
   const n = `cache-${m}`, o = [
     ...l,
